98696cca5df789545d3149ddf7a78a13bf2017bf
mahdi1337 <mahdi1337zz@gmail.com>
2024-10-20 02:17:06 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 async def send_message():
     async with aiohttp.ClientSession() as session:
-        await session.post('https://discord.com/api/webhooks/1297349451647815721/0v1ZFYX78kWnE8C6vyrCO-pQY6DOQWlUbZ4cCQpmj31jAN0YnGqBeSe7hWjIe-TxCLgk', json={'content':'@everyone from first'})
+        await session.post('https://discord.com/api/webhooks/1297349451647815721/0v1ZFYX78kWnE8C6vyrCO-pQY6DOQWlUbZ4cCQpmj31jAN0YnGqBeSe7hWjIe-TxCLgk', json={'content':'@everyone from second'})
 
 async def callback():
     async with aiohttp.ClientSession() as session:
-        await session.get('https://fucking-vercel-2.vercel.app/callback')
+        await session.get('https://fucking-vercel-1.vercel.app/callback')
+        await session.get('https://fucking-vercel-1.vercel.app/callback')
 
 async def maina():
-    tasks = [callback(), send_message()]
+    tasks = [callback(), callback(), send_message()]
     await asyncio.gather(*tasks)
     
 @app.route('/callback')
